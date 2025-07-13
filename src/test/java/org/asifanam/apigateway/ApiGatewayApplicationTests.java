928de5a931b89fd18b709f53645f0eeda025400e
Asif Anam <anam1828@cseku.ac.bd>
2025-07-13 11:59:46 +0600

    first commit

---
+++ src/test/java/org/asifanam/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.asifanam.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
